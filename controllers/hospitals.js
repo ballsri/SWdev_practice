62664e78bdb47094be6bb7b577b8b074e0ede074
ballsri <ball04407@hotmail.com>
2023-03-15 23:52:57 +0700

    add appointment crud with authorization

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -1,4 +1,4 @@
- const Hospital = require('../models/hospital')
+ const Hospital = require('../models/Hospital')
 
 
 //@desc     Get all hospitals
@@ -6,8 +6,62 @@
 //@acess    Public
 exports.getHospitals= async (req,res,next)=>{
     try{
-        const hospitals = await Hospital.find();
-        res.status(200).json({success:true, data:hospitals});
+        let query;
+        const reqQuery = {...req.query};
+        const removeFields = ['select','sort', 'page', 'limit'];
+
+        removeFields.forEach(param => delete reqQuery[param]);
+
+        let queryStr = JSON.stringify(reqQuery);
+
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+        query = Hospital.find(JSON.parse(queryStr)).populate('appointments');
+
+        if (req.query.select) {
+            const fields = req.query.select.split(',').join(' ');
+            query = query.select(fields);
+        }
+
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort('-createdAt');
+        }
+
+        const page = parseInt(req.query.page, 10) || 1;
+        const limit = parseInt(req.query.limit, 10) || 25;
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+        const total = await Hospital.countDocuments();
+
+        query = query.skip(startIndex).limit(limit);
+
+        const hospitals = await query;
+
+        const pagination = {};
+
+        if (endIndex < total) {
+            pagination.next = {
+                page: page + 1,
+                limit
+            }
+        }
+
+        if (startIndex > 0) {
+            pagination.prev = {
+                page: page - 1,
+                limit
+            }
+        }
+
+
+        res.status(200).json({
+            success:true,
+            count:hospitals.length,
+            pagination,
+            data:hospitals
+        });
     } catch (err) {
         res.status(400).json({success:false});
     }
@@ -62,10 +116,11 @@ exports.updateHospital= async (req,res,next)=>{
 //@acess    Private
 exports.deleteHospital= async (req,res,next)=>{
     try{
-        const hospital = await Hospital.findByIdAndDelete(req.params.id);
+        const hospital = await Hospital.findById(req.params.id);
         if ( !hospital )  {
             res.status(400).json({success:false});    
         }
+        hospital.remove();
         res.status(200).json({success:true, data:hospital});
     } catch (err) {
         res.status(400).json({success:false});
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ connectDB();
 
 //Route files
 const hospitals = require('./routes/hospitals');
+const appointments = require('./routes/appointments');
 const auth = require('./routes/auth');
 
 
@@ -25,6 +26,7 @@ app.use(cookieParser());
 
 app.use('/api/v1/hospitals/', hospitals)
 app.use('/api/v1/auth/', auth)
+app.use('/api/v1/appointments/', appointments)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log("VacQ is running in ", process.env.NODE_ENV, ' mode on port ', PORT));
